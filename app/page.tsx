771ae20e27faff276d587235ca82e4281ca66dd6
Fvrenn <hegetimothe@gmail.com>
2025-05-29 15:29:17 +0200

    add panel for skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,113 @@
 "use client";
-
+import { ArrowLeft } from "lucide-react";
 import ThreeScene from "../public/3D/ThreeScene";
 import { Progress } from "@heroui/react";
+import { useState } from "react";
+import { Checkbox } from "@heroui/react";
+export interface Competence {
+  id: string;
+  description: string;
+  isCompleted: boolean;
+  files?: File[];
+}
+
+export interface Badge {
+  id: string;
+  name: string;
+  imageSrc: string;
+  competences: Competence[];
+}
+
+const badgesData: Badge[] = [
+  {
+    id: "2b-spe_PF",
+    name: "Spécialité PF",
+    imageSrc: "/etape-badges/2b-spe_PF.svg",
+    competences: [
+      {
+        id: "b1",
+        description:
+          'Acquérir et savoir utiliser le "Guide du Bois" (p. 9 à 11)',
+        isCompleted: false,
+      },
+      {
+        id: "b2",
+        description:
+          "Se repérer dans le carnet et savoir expliquer l'ordre et le principe des différentes parties de chaque volume.",
+        isCompleted: false,
+      },
+      {
+        id: "b3",
+        description:
+          "Lire le chapitre \"L'enfant à l'âge PF\" p.19 du Guide du Bois et animer une discussion avec la maîtrise pour adapter les activités et les attitudes des Chefs.",
+        isCompleted: false,
+      },
+      {
+        id: "b4",
+        description:
+          "Observer les jeunes de ta sizaine, noter pour chacun d'eux les domaines dans lesquels il peut progresser (gestion affaires perso, vie de groupe, une des 5 relations ...), proposer des activités en rapport et faire le point à la fin du trimestre.",
+        isCompleted: false,
+      },
+      {
+        id: "b5",
+        description:
+          "Connaître les grandes lignes de l'histoire des ABQS, le rôle des 5 personnages principaux et savoir raconter le départ et l'arrivée au Parc.",
+        isCompleted: false,
+      },
+      {
+        id: "b6",
+        description:
+          "Expliquer aux jeunes le sens des différents rituels (rassemblement, Grand Arbre, Foulard d'Accueil...) et connaître la place des différents marqueurs sur l'uniforme.",
+        isCompleted: false,
+      },
+      {
+        id: "b7",
+        description:
+          "Accompagner un ami du Bois dans toute la démarche de la Parole de PF.",
+        isCompleted: false,
+      },
+    ],
+  },
+  {
+    id: "2c-spe_F",
+    name: "Spécialité F",
+    imageSrc: "/etape-badges/2c-spe_F.svg",
+    competences: [
+      {
+        id: "f1",
+        description: "Maîtriser les techniques de base du feu",
+        isCompleted: false,
+      },
+      {
+        id: "f2",
+        description: "Connaître les règles de sécurité",
+        isCompleted: false,
+      },
+    ],
+  },
+];
 
 export default function Home() {
+  const [selectedBadge, setSelectedBadge] = useState<Badge | null>(null);
+  const [showProgress, setShowProgress] = useState(true);
+
+  const handleBackToProgress = () => {
+    setSelectedBadge(null);
+    setShowProgress(true);
+  };
+
+  const handleBadgeClick = (badgeId: string) => {
+    if (selectedBadge && selectedBadge.id === badgeId) {
+      handleBackToProgress();
+      return;
+    }
+
+    const badge = badgesData.find((b) => b.id === badgeId);
+    setSelectedBadge(badge || null);
+    setShowProgress(false);
+  };
   return (
-    <div className="page__container">
+    <div className={`page__container ${selectedBadge ? "badge-active" : ""}`}>
       <div className="panel--primary">
         <div className="panel--primary__content">
           <div className="panel-border__top-left"></div>
@@ -35,63 +137,13 @@ export default function Home() {
 
               <div className="content__badges">
                 <div className="holographic-container">
-                  <div className="holographic-card">
+                  <div
+                    className={`holographic-card ${
+                      selectedBadge?.id === "2b-spe_PF" ? "active" : ""
+                    }`}
+                    onClick={() => handleBadgeClick("2b-spe_PF")}
+                  >
                     <img src="/etape-badges/2b-spe_PF.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2c-spe_F.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2e-spe_animation.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2f-spe_communication.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2g-spe_construction.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2h-spe_cuisine.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2i-spe_explo.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2j-spe_intendance.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2k-spe_materiel.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2l-spe_nature.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2m-sante.svg" alt="" />
-                  </div>
-                </div>
-                <div className="holographic-container">
-                  <div className="holographic-card">
-                    <img src="/etape-badges/2n-spe_vie_spi.svg" alt="" />
                   </div>
                 </div>
               </div>
@@ -120,16 +172,61 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="panel--secondary">
+      <div className="panel--secondary purple ">
         <div className="panel--secondary__content items-center justify-center relative">
           <div className="border-right"></div>
           <div className="border-right2"></div>
-          <h2 className="font-koulen text-2xl text-white font-bold tracking-wide">
-            MA PROGRESSION
-          </h2>
-          <div>
-            <Progress value={60} color="warning" size="md" />
-          </div>
+
+          {/* Affichage conditionnel : Progression OU Compétences */}
+          {showProgress ? (
+            <div className="progress-view p-4">
+              <h2 className="font-koulen text-2xl text-white font-bold tracking-wide text-left">
+                MA PROGRESSION
+              </h2>
+              <div>
+                <Progress value={60} color="warning" size="md" />
+              </div>
+            </div>
+          ) : (
+            <div className="competences-view p-4">
+              <div className="flex items-center mb-4 gap-3">
+                <button onClick={handleBackToProgress}>
+                  <ArrowLeft className="text-white" />
+                </button>
+
+                <h2 className="font-koulen text-2xl text-white font-bold tracking-wide">
+                  {selectedBadge?.name}
+                </h2>
+              </div>
+              <div className="competences-list space-y-3">
+                {selectedBadge?.competences.map((competence) => (
+                  <div key={competence.id} className="competence-item">
+                    <label className="flex gap-3 text-white text-sm">
+                      <Checkbox
+                        defaultSelected
+                        classNames={{
+                          base: "max-w-full",
+                          wrapper: [
+                            "before:border-white before:bg-white",
+                            "after:bg-[#594238] after:border-[#594238]",
+                            "hover:before:border-white/70",
+                          ].join(" "),
+                          icon: "text-white",
+                          label: "text-white font-medium",
+                        }}
+                      >
+                        <span className="text-checkbox font-DMSans">
+                          {competence.description}
+                        </span>
+                      </Checkbox>
+                      ;
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+
           <div className="border-right-center"></div>
           <div className="border-right-center2"></div>
         </div>
