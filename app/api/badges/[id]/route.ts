f93288e092a7f72fc44c4b6f2ba392f35ae9bb79
Fvrenn <hegetimothe@gmail.com>
2025-06-01 17:47:49 +0200

    rm patch

--- app/api/badges/[id]/route.ts
+++ app/api/badges/[id]/route.ts
@@ -3,28 +3,28 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function PATCH(request: Request, { params }: { params: { id: string } }) {
-  const data = await request.json();
-  await prisma.competences.deleteMany({ where: { badge_id: Number(params.id) } });
-  await prisma.realisations.deleteMany({ where: { badge_id: Number(params.id) } });
-  const badge = await prisma.badges.update({
-    where: { id: Number(params.id) },
-    data: {
-      competences: {
-        create: data.competences || [],
-      },
-      realisations: {
-        create: data.realisations || [],
-      },
-    },
-    include: {
-      competences: true,
-      realisations: true,
-    },
-  });
+// export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+//   const data = await request.json();
+//   await prisma.competences.deleteMany({ where: { badge_id: Number(params.id) } });
+//   await prisma.realisations.deleteMany({ where: { badge_id: Number(params.id) } });
+//   const badge = await prisma.badges.update({
+//     where: { id: Number(params.id) },
+//     data: {
+//       competences: {
+//         create: data.competences || [],
+//       },
+//       realisations: {
+//         create: data.realisations || [],
+//       },
+//     },
+//     include: {
+//       competences: true,
+//       realisations: true,
+//     },
+//   });
 
-  return NextResponse.json(badge);
-}
+//   return NextResponse.json(badge);
+// }
 
 
 // export async function DELETE(request: Request, { params }: { params: { id: string } }) {
