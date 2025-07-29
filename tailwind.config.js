b0e8a06a4003633948a3954457a0db084f16c2a2
Fvrenn <hegetimothe@gmail.com>
2025-07-29 22:45:57 +0200

    fix style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 import {heroui} from "@heroui/theme"
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
-  ],
+content: [
+  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  './app/**/*.{js,ts,jsx,tsx,mdx}',
+  "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+],
   theme: {
     extend: {
       fontFamily: {
