e536620331958e1a7cc9ba64ff05c47b5f5e265d
Fvrenn <hegetimothe@gmail.com>
2026-04-26 19:32:11 +0200

    increase server action body size limit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from 'next';
-
 const nextConfig: NextConfig = {
     experimental: {
         authInterrupts: true,
+        serverActions: {
+            bodySizeLimit: '10mb'
+        }
     }
 };
-
 export default nextConfig;
