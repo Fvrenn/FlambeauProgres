d3c84e378a572f92ccdaf76fa04843883dbf7fb6
Fvrenn <hegetimothe@gmail.com>
2026-06-16 23:06:54 +0200

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        '/((?!api/auth|login|_next/static|_next/image|favicon.ico).*)'
+        '/((?!api/auth|login|signup|_next/static|_next/image|favicon.ico).*)'
     ],
 };
