b9030467444e26f83175654b6f59b003e2dfad9f
Fvrenn <hegetimothe@gmail.com>
2026-06-17 00:11:06 +0200

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-    const sessionCookie = getSessionCookie(request);
+    const sessionCookie = getSessionCookie(request, {
+        cookiePrefix: "better-auth"
+    });
 
     if (!sessionCookie) {
         const url = request.nextUrl.clone();
